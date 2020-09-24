58537449f41f2ba603a330976c90a09930ffe773
Jeff Liu <jeff.liu@outlook.co.nz>
2020-09-25 11:00:49 +1200

    part 7: use session to enable access times

--- catalog/views.py
+++ catalog/views.py
@@ -27,12 +27,15 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(
         status__exact='a').count()
     num_authors = Author.objects.count()  # The 'all()' is implied by default.
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
 
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books': num_books, 'num_instances': num_instances,
+        context={'num_books': num_books, 'num_instances': num_instances, 'num_visits':num_visits,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors},
     )
 
