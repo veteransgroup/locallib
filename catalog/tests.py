e14b698f4cdebb080da255f5848326278628aaaf
Jeff Liu <jeff.liu@outlook.co.nz>
2020-09-26 13:52:44 +1200

    part 10. django auto tests

--- catalog/tests.py
+++ catalog/tests.py
@@ -7,6 +7,29 @@ from django.urls import reverse
 from django.contrib.auth.models import Permission
 # Create your tests here.
 
+class YourTestClass(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass
+
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+        pass
+
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
+
+    def test_false_is_true(self):
+        print("Method: test_false_is_true.")
+        self.assertTrue(False)
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
+
 class AuthorModelTest(TestCase):
 
     @classmethod
@@ -22,7 +45,7 @@ class AuthorModelTest(TestCase):
     def test_date_of_death_label(self):
         author=Author.objects.get(id=1)
         field_label = author._meta.get_field('date_of_death').verbose_name
-        self.assertEquals(field_label,'died')
+        self.assertEquals(field_label,'Died')
 
     def test_first_name_max_length(self):
         author=Author.objects.get(id=1)
